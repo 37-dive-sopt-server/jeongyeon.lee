2103319823b680b101a80ed448cc98749cb4386d
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-12 21:33:34 +0900

    [Feat] Gender 입력값이 비어있는 경우 예외 메시지 추가

--- src/main/java/org/sopt/constant/ErrorMessage.java
+++ src/main/java/org/sopt/constant/ErrorMessage.java
@@ -3,6 +3,7 @@ package org.sopt.constant;
 public enum ErrorMessage {
     NAME_BLANK("이름을 입력해주세요."),
     INVALID_GENDER("성별은 MALE / FEMALE로 입력해주세요."),
+    GENDER_BLANK("성별을 입력해주세요."),
     INVALID_EMAIL("이메일 형식으로 작성해주세요."),
     EMAIL_BLANK("이메일을 입력해주세요.")
     ;
