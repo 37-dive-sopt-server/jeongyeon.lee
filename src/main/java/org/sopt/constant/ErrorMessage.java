aab5da21d3dc34337c55a798fd652677e179bab8
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-12 21:46:46 +0900

    [Feat] ErrorMessage에 이메일 중복 여부 추가

--- src/main/java/org/sopt/constant/ErrorMessage.java
+++ src/main/java/org/sopt/constant/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     INVALID_GENDER("성별은 MALE / FEMALE로 입력해주세요."),
     GENDER_BLANK("성별을 입력해주세요."),
     INVALID_EMAIL("이메일 형식으로 작성해주세요."),
-    EMAIL_BLANK("이메일을 입력해주세요.")
+    EMAIL_BLANK("이메일을 입력해주세요."),
+    DUPLICATE_EMAIL("중복된 이메일입니다."),
     ;
 
     private final String message;
