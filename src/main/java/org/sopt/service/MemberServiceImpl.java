351f6234bc41204265f511f7dc92359529006ac1
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-12 20:50:32 +0900

    [Chore] 회원 생성 비즈니스 로직 파라미터 수정

--- src/main/java/org/sopt/service/MemberServiceImpl.java
+++ src/main/java/org/sopt/service/MemberServiceImpl.java
@@ -1,8 +1,10 @@
 package org.sopt.service;
 
+import org.sopt.domain.Gender;
 import org.sopt.domain.Member;
 import org.sopt.repository.MemoryMemberRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,9 +13,12 @@ public class MemberServiceImpl implements MemberService {
     private final MemoryMemberRepository memberRepository =  new MemoryMemberRepository();
     private static long sequence = 1L;
 
-    public Long join(String name) {
+    public Long join(String name, int birthYear, int birthMonth, int birthDay, String email, String gender) {
 
-        Member member = new Member(sequence++, name);
+        Member member = new Member(sequence++, name,
+                LocalDate.of(birthYear, birthMonth, birthDay),
+                email,
+                Gender.valueOf(gender));
         memberRepository.save(member);
         return member.getId();
     }
