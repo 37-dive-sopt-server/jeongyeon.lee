68790992585f3aaa30dc9058a6f91ca534587aeb
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-14 16:59:17 +0900

    [Chore] MemberRepository에 접근 제어자 제거

--- src/main/java/org/sopt/repository/MemberRepository.java
+++ src/main/java/org/sopt/repository/MemberRepository.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    public Member save(Member member);
+    Member save(Member member);
 
-    public Optional<Member> findById(Long id);
+    Optional<Member> findById(Long id);
 
-    public List<Member> findAll();
+    List<Member> findAll();
 
-    public boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
 
-    public void deleteById(Long id);
+    void deleteById(Long id);
 }
