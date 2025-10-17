9fda281e196e5b2a26cd47e1ad6285f5b6ff093b
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-17 21:10:24 +0900

    [Chore] 이름 유효성 검사 시 커스텀 예외 처리하도록 수정

--- src/main/java/org/sopt/util/validator/MemberNameValidator.java
+++ src/main/java/org/sopt/util/validator/MemberNameValidator.java
@@ -1,11 +1,13 @@
 package org.sopt.util.validator;
 
+import org.sopt.exception.customexception.BadRequestException;
+
 import static org.sopt.exception.constant.ErrorMessage.NAME_BLANK;
 
 public class MemberNameValidator {
     public static void validateName(String name) {
         if(name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException(NAME_BLANK.getMessage());
+            throw new BadRequestException(NAME_BLANK.getMessage());
         }
     }
 }
