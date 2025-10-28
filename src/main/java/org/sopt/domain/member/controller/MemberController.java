7108801d0b52f0d8d246d68aa878b8dc18356c23
dlwjddus1112 <dlwjddus1112@naver.com>
2025-10-28 14:51:04 +0900

    [Feat] 입력 유효성 처리를 DTO에서 처리하도록 변경

--- src/main/java/org/sopt/domain/member/controller/MemberController.java
+++ src/main/java/org/sopt/domain/member/controller/MemberController.java
@@ -1,12 +1,10 @@
 package org.sopt.domain.member.controller;
 
+import jakarta.validation.Valid;
 import org.sopt.domain.member.dto.request.MemberCreateRequest;
 import org.sopt.domain.member.dto.response.MemberDetailResponse;
 import org.sopt.domain.member.service.MemberService;
 import org.sopt.global.response.BaseResponse;
-import org.sopt.global.validator.EmailValidator;
-import org.sopt.global.validator.GenderValidator;
-import org.sopt.global.validator.MemberNameValidator;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,15 +20,9 @@ public class MemberController {
     }
 
     @PostMapping
-    public BaseResponse<Long> createMember(MemberCreateRequest request) {
-
-        MemberNameValidator.validateName(request.name());
-
-        EmailValidator.validateEmail(request.email());
-
-        GenderValidator.validateGender(request.gender());
-
-        return BaseResponse.create(memberService.join(request.name(), request.birthDate(), request.email(), request.gender()),"회원 생성 완료");
+    public BaseResponse<Long> createMember(@Valid @RequestBody MemberCreateRequest request) {
+        return BaseResponse.create(memberService.join(request.name(),
+                request.birthDate(), request.email(), request.gender()),"회원 생성 완료");
     }
 
     @GetMapping("{memberId}")
--- src/main/java/org/sopt/domain/member/dto/request/MemberCreateRequest.java
+++ src/main/java/org/sopt/domain/member/dto/request/MemberCreateRequest.java
@@ -1,9 +1,20 @@
 package org.sopt.domain.member.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import org.sopt.global.annotation.ValidEmail;
+import org.sopt.global.annotation.ValidGender;
+
 public record MemberCreateRequest(
+        @NotBlank(message = "이름은 필수 입력 사항입니다.")
         String name,
+
+        @NotBlank(message = "생년월일은 필수 입력 사항입니다.")
         String birthDate,
+
+        @ValidEmail
         String email,
+
+        @ValidGender
         String gender
 ) {
 }
