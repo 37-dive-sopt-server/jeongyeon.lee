d86524c9e3844843dd54a6c0e8a894b687ba3253
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-03 13:59:24 +0900

    [Feat] Article 엔티티 Controller, Repository, Service 추가

---
+++ src/main/java/org/sopt/domain/article/controller/ArticleController.java
@@ -0,0 +1,11 @@
+package org.sopt.domain.article.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("articles")
+@RequiredArgsConstructor
+public class ArticleController {
+}
---
+++ src/main/java/org/sopt/domain/article/service/ArticleService.java
@@ -0,0 +1,11 @@
+package org.sopt.domain.article.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ArticleService {
+}
