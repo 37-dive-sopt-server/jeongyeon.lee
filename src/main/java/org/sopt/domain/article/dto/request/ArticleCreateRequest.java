4d41e532037a2c177225fa643344dd440755bf1f
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-03 14:56:31 +0900

    [Feat] 게시글 생성 요청 DTO 작성

---
+++ src/main/java/org/sopt/domain/article/dto/request/ArticleCreateRequest.java
@@ -0,0 +1,20 @@
+package org.sopt.domain.article.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.sopt.global.annotation.ValidTag;
+
+public record ArticleCreateRequest(
+        @NotNull
+        Long memberId,
+
+        @NotBlank(message = "제목은 비어있을 수 없습니다.")
+        String title,
+
+        @NotBlank(message = "내용은 비어있을 수 없습니다.")
+        String content,
+
+        @ValidTag
+        String tag
+) {
+}
