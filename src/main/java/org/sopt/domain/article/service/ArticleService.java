96fef633293f39243eba5a50e035862c79421d0e
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-03 14:57:40 +0900

    [Feat] 게시글 생성 비즈니스 로직 구현

--- src/main/java/org/sopt/domain/article/service/ArticleService.java
+++ src/main/java/org/sopt/domain/article/service/ArticleService.java
@@ -1,11 +1,42 @@
 package org.sopt.domain.article.service;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.domain.article.constant.ArticleTag;
+import org.sopt.domain.article.entity.Article;
+import org.sopt.domain.article.repository.ArticleRepository;
+import org.sopt.domain.member.entity.Member;
+import org.sopt.domain.member.service.MemberServiceImpl;
+import org.sopt.global.exception.customexception.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.sopt.domain.article.errorcode.ArticleErrorCode.ARTICLE_TITLE_DUPLICATE;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    private final MemberServiceImpl memberService;
+
+    public Long createArticle(Long memberId, String title, String content, String tag){
+        checkTitleDuplicate(title);
+
+        Member member = memberService.findById(memberId);
+
+        Article article = Article.create(title, content, ArticleTag.valueOf(tag), member);
+        articleRepository.save(article);
+
+        return article.getId();
+    }
+
+    private void checkTitleDuplicate(String title) {
+        if(articleRepository.existsByTitle(title)){
+            throw new BadRequestException(ARTICLE_TITLE_DUPLICATE);
+        }
+    }
+
+
 }
