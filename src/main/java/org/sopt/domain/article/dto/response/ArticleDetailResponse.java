5dd6d562c253beb051ad877dc0b675eeea32fea2
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-03 16:27:49 +0900

    [Chore] 아티클 조회 응답 DTO에 필드 추가

--- src/main/java/org/sopt/domain/article/dto/response/ArticleDetailResponse.java
+++ src/main/java/org/sopt/domain/article/dto/response/ArticleDetailResponse.java
@@ -5,14 +5,19 @@ import org.sopt.domain.article.entity.Article;
 import java.time.LocalDateTime;
 
 public record ArticleDetailResponse(
+        Long articleId,
         Long memberId,
+        String authorName,
         String title,
         String content,
         String tag,
         LocalDateTime createdAt
 ) {
     public static ArticleDetailResponse from(Article article){
-        return new ArticleDetailResponse(article.getMember().getId(),
+        return new ArticleDetailResponse(
+                article.getId(),
+                article.getMember().getId(),
+                article.getMember().getName(),
                 article.getTitle(),
                 article.getContent(),
                 article.getTag().toString(),
