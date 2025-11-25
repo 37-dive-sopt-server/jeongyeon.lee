7a27e159214d2fbdb2809e4f41ba16bb082db088
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-25 16:29:42 +0900

    [Feat] RefreshToken 엔티티 구현

---
+++ src/main/java/org/sopt/domain/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package org.sopt.domain.auth.repository;
+
+import org.sopt.domain.auth.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+}
