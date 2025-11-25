d846096941040486a98bbb0273b5e599b31fb7b7
dlwjddus1112 <dlwjddus1112@naver.com>
2025-11-25 16:44:17 +0900

    [Feat] 토큰 재발급 로직 구현

--- src/main/java/org/sopt/domain/auth/service/AuthService.java
+++ src/main/java/org/sopt/domain/auth/service/AuthService.java
@@ -8,12 +8,16 @@ import org.sopt.domain.member.entity.Member;
 import org.sopt.domain.member.repository.MemberRepository;
 import org.sopt.global.exception.customexception.CustomException;
 import org.sopt.global.jwt.JwtProvider;
+import org.sopt.global.jwt.JwtUtil;
+import org.sopt.global.jwt.constant.HttpHeaderConstants;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.sopt.domain.auth.errorcode.AuthErrorCode.*;
 import static org.sopt.domain.member.errorcode.MemberErrorCode.MEMBER_NOT_FOUND;
 import static org.sopt.domain.member.errorcode.MemberErrorCode.PASSWORD_MISMATCH;
+import static org.sopt.global.exception.errorcode.GlobalErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +29,10 @@ public class AuthService {
     private final MemberRepository memberRepository;
 
     private final RefreshTokenRepository refreshTokenRepository;
+
     private final PasswordEncoder passwordEncoder;
+
+    private final JwtUtil jwtUtil;
 
     public TokenResponse login(String email, String password) {
 
@@ -36,7 +43,7 @@ public class AuthService {
         String accessToken = jwtProvider.generateAccessToken(member.getId());
         String refreshToken = jwtProvider.generateRefreshToken(member.getId());
 
-        refreshTokenRepository.findByUserId(member.getId())
+        refreshTokenRepository.findByMemberId(member.getId())
                 .ifPresentOrElse(
                         existing -> existing.updateToken(refreshToken),
                         () -> refreshTokenRepository.save(RefreshToken.of(member.getId(), refreshToken))
@@ -50,6 +57,29 @@ public class AuthService {
         if(!passwordEncoder.matches(password, member.getPassword())) {
             throw new CustomException(PASSWORD_MISMATCH);
         }
+    }
+
+    public TokenResponse reissue(String bearerToken) {
+        String refreshToken = bearerToken.replace(HttpHeaderConstants.BEARER_PREFIX, "").trim();
+        if(!jwtUtil.isTokenValid(refreshToken)) {
+            throw new CustomException(JWT_INVALID);
+        }
+
+        Long memberId = jwtUtil.extractMemberIdFromToken(refreshToken);
+
+        RefreshToken savedToken = refreshTokenRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new CustomException(REFRESH_NOT_FOUND));
+
+        if (!savedToken.getRefreshToken().equals(refreshToken)) {
+            throw new CustomException(REFRESH_MISMATHCES);
+        }
+
+        String newAccessToken = jwtProvider.generateAccessToken(memberId);
+        String newRefreshToken = jwtProvider.generateRefreshToken(memberId);
+
+        savedToken.updateToken(newRefreshToken);
+
+        return TokenResponse.of(memberId, newAccessToken, newRefreshToken);
     }
 
 
