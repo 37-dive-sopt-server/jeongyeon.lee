21b3ce9416f7091d8c30d7bec02de45bbd5c655f
dlwjddus1112 <dlwjddus1112@naver.com>
2025-12-03 00:18:07 +0900

    [Feat] 댓글 응답 DTO에 작성자 ID 추가

--- src/main/java/org/sopt/domain/comment/dto/response/CommentResponse.java
+++ src/main/java/org/sopt/domain/comment/dto/response/CommentResponse.java
@@ -7,10 +7,13 @@ public record CommentResponse(
         @Schema(description = "댓글 ID", example = "1")
         Long commentId,
 
+        @Schema(description = "작성자 ID", example = "1")
+        Long writerId,
+
         @Schema(description = "댓글 내용", example = "ㅎㅇㅎㅇ")
         String content
 ) {
     public static CommentResponse from(Comment comment) {
-        return new CommentResponse(comment.getId(), comment.getContent());
+        return new CommentResponse(comment.getId(),comment.getMember().getId(), comment.getContent());
     }
 }
