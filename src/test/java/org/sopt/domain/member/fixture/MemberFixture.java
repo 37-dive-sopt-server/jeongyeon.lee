94d4d6217b0ac6756da6c5d76a159cdfd8b6cfc5
dlwjddus1112 <dlwjddus1112@naver.com>
2025-12-04 22:11:19 +0900

    [Feat] 도메인별 Fixture 구현

---
+++ src/test/java/org/sopt/domain/member/fixture/MemberFixture.java
@@ -0,0 +1,28 @@
+package org.sopt.domain.member.fixture;
+
+import org.sopt.domain.member.constant.Gender;
+import org.sopt.domain.member.entity.Member;
+
+import java.time.LocalDate;
+
+public class MemberFixture {
+
+    public static final String MEMBER_EMAIL = "test@test.com";
+    public static final String MEMBER_PASSWORD = "test";
+    public static final String MEMBER_NAME = "test";
+
+    public static final LocalDate MEMBER_DEFAULT_BIRTHDATE = LocalDate.of(2000,11,12);
+    public static final LocalDate MEMBER_YOUNG_BIRTHDATE = LocalDate.of(2010,11,12);
+
+
+    public static Member getmember(LocalDate birthDate){
+        return Member.builder()
+                .password(MEMBER_PASSWORD)
+                .name(MEMBER_NAME)
+                .birthDate(birthDate)
+                .email(MEMBER_EMAIL)
+                .gender(Gender.MALE)
+                .build();
+    }
+
+}
