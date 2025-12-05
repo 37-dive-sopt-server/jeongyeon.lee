abe1d9e84c09ef4b2b0616e3f14e8a678383fadd
dlwjddus1112 <dlwjddus1112@naver.com>
2025-12-05 16:27:09 +0900

    [Chore] MemberController 리턴 타입 변경

--- src/main/java/org/sopt/domain/member/controller/MemberController.java
+++ src/main/java/org/sopt/domain/member/controller/MemberController.java
@@ -7,6 +7,8 @@ import org.sopt.domain.member.dto.response.MemberDetailResponse;
 import org.sopt.domain.member.dto.response.MemberListResponse;
 import org.sopt.domain.member.service.MemberService;
 import org.sopt.global.response.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,23 +19,26 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public BaseResponse<Long> createMember(@Valid @RequestBody MemberCreateRequest request) {
-        return BaseResponse.create(memberService.join(request.toCommand()),"회원 생성 완료");
+    public ResponseEntity<BaseResponse<Long>> createMember(@Valid @RequestBody MemberCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.create(memberService.join(request.toCommand()),"회원 생성 완료"));
     }
 
     @GetMapping("{memberId}")
-    public BaseResponse<MemberDetailResponse> findMemberById(@PathVariable Long memberId) {
-        return BaseResponse.ok(memberService.getMemberDetail(memberId), "회원 조회 완료");
+    public ResponseEntity<BaseResponse<MemberDetailResponse>> findMemberById(@PathVariable Long memberId) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(memberService.getMemberDetail(memberId), "회원 조회 완료"));
     }
 
     @GetMapping()
-    public BaseResponse<MemberListResponse> getAllMembers() {
-        return BaseResponse.ok(memberService.findAllMembers(),"회원 목록 조회 완료");
+    public ResponseEntity<BaseResponse<MemberListResponse>> getAllMembers() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(memberService.findAllMembers(),"회원 목록 조회 완료"));
     }
 
     @DeleteMapping("{memberId}")
-    public BaseResponse<Void> deleteMember(@PathVariable Long memberId) {
+    public ResponseEntity<BaseResponse<Void>> deleteMember(@PathVariable Long memberId) {
         memberService.deleteMember(memberId);
-        return BaseResponse.ok("회원 삭제 완료");
+        return ResponseEntity.status(HttpStatus.OK).body(BaseResponse.ok("회원 삭제 완료"));
     }
 }
