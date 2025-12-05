21e1fe1ee42f6e77a4b8226ed15205266bc4b5a7
dlwjddus1112 <dlwjddus1112@naver.com>
2025-12-05 14:12:29 +0900

    [Test] Member 도메인 Business Layer 테스트 작성

--- src/test/java/org/sopt/domain/member/service/MemberServiceImplTest.java
+++ src/test/java/org/sopt/domain/member/service/MemberServiceImplTest.java
@@ -1,11 +1,11 @@
 package org.sopt.domain.member.service;
 
-import org.assertj.core.api.Assertions;
 import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sopt.domain.member.constant.Gender;
+import org.sopt.domain.member.dto.response.MemberDetailResponse;
 import org.sopt.domain.member.dto.response.MemberListResponse;
 import org.sopt.domain.member.entity.Member;
 import org.sopt.domain.member.fixture.MemberFixture;
@@ -21,8 +21,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.sopt.domain.member.errorcode.MemberErrorCode.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -45,6 +48,104 @@ class MemberServiceImplTest {
         });
     }
 
+    @DisplayName("")
+    @Test
+    void join(){
+       //given
+        MemberCreateCommand command = MemberCreateCommand.builder()
+                .email("test1@test.com")
+                .name(MemberFixture.MEMBER_NAME)
+                .password(MemberFixture.MEMBER_PASSWORD)
+                .birthDate(MemberFixture.MEMBER_DEFAULT_BIRTHDATE.toString())
+                .gender(Gender.MALE.toString())
+                .build();
+
+       //when
+        Long memberId = memberServiceImpl.join(command);
+
+        //then
+        assertThat(memberId).isEqualTo(1L);
+    }
+
+    @DisplayName("회원가입 시 이메일이 중복되면 예외가 발생한다.")
+    @Test
+    void joinWithDuplicateEmail(){
+       //given
+        Member member1 = MemberFixture.getmember(MemberFixture.MEMBER_DEFAULT_BIRTHDATE);
+        memberRepository.save(member1);
+
+        MemberCreateCommand command = MemberCreateCommand.builder()
+                .email(MemberFixture.MEMBER_EMAIL)
+                .name(MemberFixture.MEMBER_NAME)
+                .password(MemberFixture.MEMBER_PASSWORD)
+                .birthDate(MemberFixture.MEMBER_DEFAULT_BIRTHDATE.toString())
+                .gender(Gender.MALE.toString())
+                .build();
+        //when && then
+        assertThatThrownBy(() -> memberServiceImpl.join(command)).hasMessage(DUPLICATE_EMAIL.getMessage());
+
+    }
+
+    @DisplayName("20세 미만 회원이 회원 가입 시 예외가 발생한다.")
+    @Test
+    void joinWithLowAge(){
+       //given
+        MemberCreateCommand command = MemberCreateCommand.builder()
+                .email(MemberFixture.MEMBER_EMAIL)
+                .name(MemberFixture.MEMBER_NAME)
+                .password(MemberFixture.MEMBER_PASSWORD)
+                .birthDate(MemberFixture.MEMBER_YOUNG_BIRTHDATE.toString())
+                .gender(Gender.MALE.toString())
+                .build();
+       //when && then
+        assertThatThrownBy(() -> memberServiceImpl.join(command)).hasMessage(MEMBER_AGE_TOO_LOW.getMessage());
+
+    }
+
+    @DisplayName("회원의 ID로 회원을 삭제할 수 있다.")
+    @Test
+    void deleteMember(){
+       //given
+        Member member = MemberFixture.getmember(MemberFixture.MEMBER_DEFAULT_BIRTHDATE);
+        memberRepository.save(member);
+
+        //when
+        memberServiceImpl.deleteMember(member.getId());
+
+       //then
+        assertThat(memberRepository.findById(member.getId())).isEmpty();
+    }
+
+    @DisplayName("존재하지 않는 회원의 ID로 회원을 삭제하려고 하면 예외가 발생한다.")
+    @Test
+    void delteteMemberWithoutId(){
+       //given
+        Member member = MemberFixture.getmember(MemberFixture.MEMBER_DEFAULT_BIRTHDATE);
+        memberRepository.save(member);
+
+       //when && then
+        assertThatThrownBy(() -> memberServiceImpl.deleteMember(2L)).hasMessage(MEMBER_NOT_FOUND.getMessage());
+
+    }
+
+    @DisplayName("회원의 상세 정보를 조회할 수 있다.")
+    @Test
+    void getMemberDetail(){
+       //given
+        Member member = MemberFixture.getmember(MemberFixture.MEMBER_DEFAULT_BIRTHDATE);
+        memberRepository.save(member);
+
+       //when
+        MemberDetailResponse memberDetail = memberServiceImpl.getMemberDetail(member.getId());
+
+        //then
+        assertThat(memberDetail).isNotNull()
+                .extracting("name","email","birthDate")
+                .contains(
+                        MemberFixture.MEMBER_NAME, MemberFixture.MEMBER_EMAIL, MemberFixture.MEMBER_DEFAULT_BIRTHDATE
+                );
+    }
+
     @DisplayName("회원 목록을 조회할 수 있다")
     @Test
     void findAllMembers(){
@@ -56,7 +157,7 @@ class MemberServiceImplTest {
         MemberListResponse memberListResponse = memberServiceImpl.findAllMembers();
 
         //then
-        Assertions.assertThat(memberListResponse.members()).hasSize(3)
+        assertThat(memberListResponse.members()).hasSize(3)
                 .extracting("email","name")
                 .containsExactlyInAnyOrder(
                         Tuple.tuple(MemberFixture.MEMBER_EMAIL, MemberFixture.MEMBER_NAME),
@@ -106,6 +207,8 @@ class MemberServiceImplTest {
        //then
         verify(memberRepository, times(2)).findAll();
     }
+
+
 
     private void createMembers() {
         Member member1 = MemberFixture.getmember(MemberFixture.MEMBER_DEFAULT_BIRTHDATE);
