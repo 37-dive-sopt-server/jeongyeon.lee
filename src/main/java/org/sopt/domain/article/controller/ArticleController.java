2ae5baa4450b8287f4a7c707fb7c8a25254f10b2
dlwjddus1112 <dlwjddus1112@naver.com>
2025-12-08 01:28:38 +0900

    [Chore] ArticleController 반환 타입 수정

--- src/main/java/org/sopt/domain/article/controller/ArticleController.java
+++ src/main/java/org/sopt/domain/article/controller/ArticleController.java
@@ -1,6 +1,5 @@
 package org.sopt.domain.article.controller;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sopt.domain.article.constant.ArticleSearchType;
@@ -15,6 +14,8 @@ import org.sopt.domain.comment.dto.response.CommentResponse;
 import org.sopt.domain.comment.service.CommentService;
 import org.sopt.global.annotation.LoginMemberId;
 import org.sopt.global.response.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,49 +24,96 @@ import org.springframework.web.bind.annotation.*;
 public class ArticleController {
 
     private final ArticleService articleService;
-
     private final CommentService commentService;
 
     @PostMapping
-    public BaseResponse<ArticleCreateResponse> createArticle(@LoginMemberId Long memberId, @Valid @RequestBody ArticleCreateRequest req){
-        return BaseResponse.ok(articleService.createArticle(memberId, req.toCommand()),"아티클 생성이 완료되었습니다.");
+    public ResponseEntity<BaseResponse<ArticleCreateResponse>> createArticle(
+            @LoginMemberId Long memberId,
+            @Valid @RequestBody ArticleCreateRequest req
+    ){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.create(
+                        articleService.createArticle(memberId, req.toCommand()),
+                        "아티클 생성이 완료되었습니다."
+                ));
     }
 
     @GetMapping("{articleId}")
-    public BaseResponse<ArticleDetailResponse> getArticleDetail(@PathVariable Long articleId){
-        return BaseResponse.ok(articleService.getArticleDetail(articleId),"아티클 단일 조회에 성공하였습니다.");
+    public ResponseEntity<BaseResponse<ArticleDetailResponse>> getArticleDetail(
+            @PathVariable Long articleId
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(
+                        articleService.getArticleDetail(articleId),
+                        "아티클 단일 조회에 성공하였습니다."
+                ));
     }
 
-    @GetMapping()
-    public BaseResponse<ArticleListResponse> getArticleList(){
-        return BaseResponse.ok(articleService.getArticleList(),"아티클 전체 조회에 성공하였습니다.");
+    @GetMapping
+    public ResponseEntity<BaseResponse<ArticleListResponse>> getArticleList(){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(
+                        articleService.getArticleList(),
+                        "아티클 전체 조회에 성공하였습니다."
+                ));
     }
 
     @GetMapping("search")
-    public BaseResponse<ArticleListResponse> searchArticleByKeyword(@RequestParam ArticleSearchType type, @RequestParam String keyword){
-        return BaseResponse.ok(articleService.searchArticleByKeyword(type, keyword),"아티클 검색에 성공했습니다.");
+    public ResponseEntity<BaseResponse<ArticleListResponse>> searchArticleByKeyword(
+            @RequestParam ArticleSearchType type,
+            @RequestParam String keyword
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(
+                        articleService.searchArticleByKeyword(type, keyword),
+                        "아티클 검색에 성공했습니다."
+                ));
     }
 
     @PostMapping("{articleId}/comments")
-    public BaseResponse<CommentResponse> createComment(@PathVariable Long articleId, @LoginMemberId @Parameter(hidden = true) Long memberId,
-                                                       @Valid @RequestBody CommentRequest req){
-        return BaseResponse.create(commentService.createComment(articleId, memberId, req.toCommand()),"댓글 생성이 완료되었습니다.");
+    public ResponseEntity<BaseResponse<CommentResponse>> createComment(
+            @PathVariable Long articleId,
+            @LoginMemberId Long memberId,
+            @Valid @RequestBody CommentRequest req
+    ){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(BaseResponse.create(
+                        commentService.createComment(articleId, memberId, req.toCommand()),
+                        "댓글 생성이 완료되었습니다."
+                ));
     }
 
     @PatchMapping("{articleId}/{commentId}")
-    public BaseResponse<CommentResponse> updateComment(@PathVariable Long articleId, @PathVariable Long commentId,
-                                                       @Valid @RequestBody CommentRequest req){
-        return BaseResponse.ok(commentService.updateComment(articleId, commentId, req.toCommand()),"댓글 수정이 완료되었습니다.");
+    public ResponseEntity<BaseResponse<CommentResponse>> updateComment(
+            @PathVariable Long articleId,
+            @PathVariable Long commentId,
+            @Valid @RequestBody CommentRequest req
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(
+                        commentService.updateComment(articleId, commentId, req.toCommand()),
+                        "댓글 수정이 완료되었습니다."
+                ));
     }
 
     @DeleteMapping("{articleId}/{commentId}")
-    public BaseResponse<Void> deleteComment(@PathVariable Long articleId, @PathVariable Long commentId){
+    public ResponseEntity<BaseResponse<Void>> deleteComment(
+            @PathVariable Long articleId,
+            @PathVariable Long commentId
+    ){
         commentService.deleteComment(articleId, commentId);
-        return BaseResponse.ok("댓글 삭제가 완료되었습니다.");
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok("댓글 삭제가 완료되었습니다."));
     }
 
-    @GetMapping("{aritcleId}/comments")
-    public BaseResponse<CommentListResponse> getComments(@PathVariable Long aritcleId){
-        return BaseResponse.ok(commentService.findAllComments(aritcleId),"댓글 목록 조회 성공");
+    @GetMapping("{articleId}/comments")
+    public ResponseEntity<BaseResponse<CommentListResponse>> getCommentList(
+            @PathVariable Long articleId
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BaseResponse.ok(
+                        commentService.findAllComments(articleId),
+                        "댓글 목록 조회에 성공하였습니다."
+                ));
     }
 }
